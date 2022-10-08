faaa9174711c668f879bb63f116e2a9465a9daaf
ArnavVadnere <arnavvadnere@gmail.com>
2022-10-08 15:56:21 -0400

    First commit

---
+++ spotifyGesture/spotifyGesture/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for spotifyGesture project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spotifyGesture.settings")
+
+application = get_asgi_application()
